e6f9ce3595c8a62a1fa22cb39e31bf27d1031613
上将军 <tianxiaode@hotmail.com>
2026-08-03 19:04:30 +0800

    fix(logger): 补充LogEntry类型导出到index.ts

--- src/logger/index.ts
+++ src/logger/index.ts
@@ -1,4 +1,4 @@
 import { Logger } from './Logger';
-import type { LoggerOptions, ILogger, LogLevel } from './types';
+import type { LoggerOptions, ILogger, LogLevel, LogEntry } from './types';
 
-export { Logger, LoggerOptions, ILogger, LogLevel };
+export { Logger, LoggerOptions, ILogger, LogLevel, LogEntry };
