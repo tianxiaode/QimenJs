11922276391288d44679d148ac8c25c617b3800d
上将军 <tianxiaode@hotmail.com>
2026-09-11 16:20:53 +0800

    cleanup: remove compiled js files from src and showcase/src
    Keep hand-written js: src/i18n/i18n.iife.js, src/i18n/copy.js, src/i18n/locales/*.js

--- examples/showcase/src/main.js
+++
@@ -1,5 +0,0 @@
-import { ShowcaseApp } from './ShowcaseApp';
-import './register';
-import '@/theme/theme.css';
-import './showcase.css';
-new ShowcaseApp({ container: document.getElementById('app') });
